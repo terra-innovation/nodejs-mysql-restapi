0215460c7ae57362d107f5815b30b2d1c06429da
terra-innovation <jonycaleb@gmail.com>
2025-05-01 08:52:31 -0500

    NF

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,12 @@
 export default {
   verbose: true,
   testEnvironment: "node",
-  coveragePathIgnorePatterns: ["/node_modules/"],
-  transform: {},
+  coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
+  transform: {
+    "^.+\\.(ts|js)$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^#root/(.*)$": "<rootDir>/$1",
+    "^#src/(.*)$": "<rootDir>/src/$1",
+  },
 };
