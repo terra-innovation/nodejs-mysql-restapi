8c9d80316f961083b04be8519acd5ba68db70bb9
terra-innovation <jonycaleb@gmail.com>
2025-06-15 23:44:23 -0500

    configuración de pm2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "ft-api-backend",
+      script: "dist/index.js",
+      instances: 1, // puedes cambiar a 'max' para cluster mode
+      autorestart: true,
+      watch: false, // desactiva watch en producción
+      max_memory_restart: "512M",
+      env: {
+        NODE_ENV: "production",
+        TZ: "America/Lima",
+      },
+    },
+  ],
+};
