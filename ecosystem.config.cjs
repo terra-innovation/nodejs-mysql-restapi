135ecc45a3dd9138eaaf54a46dfd6ce99dfbdd75
Jony H <jonycaleb@gmail.com>
2025-12-11 16:58:15 -0500

    no es necesario definir variables de entorno desde ecosystem porque las toma de .env

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,9 +8,6 @@ module.exports = {
       max_restarts: 3,
       watch: false, // desactiva watch en producción
       max_memory_restart: "512M",
-      env: {
-        NODE_ENV: "production",
-      },
       //log_file: "logs/pm2/ft-api-backend-combined.log", // Opcional: Unifica stdout y stderr en un solo archivo
       out_file: "logs/pm2/ft-api-backend-stdout.log", // Archivo para logs de salida de console. (info, debug, trace)
       error_file: "logs/pm2/ft-api-backend-stderr.log", // Archivo para logs de error de console. (warn, error, fatal)
